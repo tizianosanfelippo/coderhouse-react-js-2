7526c0ff1c6093e968e53a369af4a8fc3ab4644c
tizianosanfelippo <agustin.sanfelippo@gmail.com>
2022-01-19 20:31:07 -0300

    Clase 3

---
+++ clase 3/src/App.js
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import NavBar from "./components/navbar/NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
+
