34d69730bb067c98ad8201191ca6f9186edc2d0f
Tiziano <agustin.sanfelippo@gmail.com>
2022-01-31 13:14:13 -0300

    Clase 4 corregida

--- clase 4/src/App.js
+++ clase 4/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <ItemListContainer />
+      <ItemListContainer nombre="Bienvenidos"/>
     </div>
   );
 }
--- clase 4/src/components/item-list-container/ItemListContainer.js
+++ clase 4/src/components/item-list-container/ItemListContainer.js
@@ -1,48 +1,19 @@
-import { Carousel } from "react-bootstrap";
-import logo from './probando.png';
+
+
+import React from 'react';
+
+function ItemListContainer(props){
+  return <div>
+Hola {props.nombre}
+  </div>;
+}
+
+export default ItemListContainer;
 
 
 
-const NavBar = () => {
-  return (
-    <Carousel>
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src={logo}
-        alt="First slide"
-      />
-      <Carousel.Caption>
-        <h3>Ejemplo 1</h3>
-        <p>Ejemplo 1</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src={logo}
-        alt="Second slide"
-      />
-  
-      <Carousel.Caption>
-        <h3>Ejemplo 2</h3>
-        <p>Ejemlo 2</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src={logo}
-        alt="Third slide"
-      />
-  
-      <Carousel.Caption>
-        <h3>Ejemplo 3</h3>
-        <p>Ejemplo 3</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-  </Carousel>
-  );
-};
 
-export default NavBar;
+
+
+
+
